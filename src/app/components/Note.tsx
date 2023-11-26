783e97ff30e3c6689838f81a6a6c0adb62c83ce4
Claudio Parra <101138305+cdp50@users.noreply.github.com>
2023-11-26 11:28:22 -0700

    we're able to delete a note

--- src/app/components/Note.tsx
+++ src/app/components/Note.tsx
@@ -18,6 +18,26 @@ const Note = ({ note }: any) => {
   // const { id: imageId, image, description: imageDescription } = images || {};
   // const { id: fileId, file, description: fileDescription } = files || {};
   const [openModal, setOpenModal] = useState(false);
+  const [isDelete, setIsDelete] = useState(false);
+
+  const deleteIt = async () => {
+    try {
+      const result = await fetch(`http://localhost:3000/api/note`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ id }),
+      });
+  
+      const answer = await result.json();
+      console.log("answer from the fetch call to delete a note at components/note", answer);
+      setIsDelete(true);
+    } catch (error) {
+      console.error('Error deleting note:', error);
+    }
+  };
+  
 
   console.log("esta es la nota que llega al note component", note);
   const twnd = "h-40 w-auto"
@@ -68,7 +88,7 @@ const Note = ({ note }: any) => {
       )}
 
       <div className='bg-white h-16 w-full absolute bottom-0 right-0 justify-end items-center  flex'>
-        <button className='hover:bg-black hover:bg-opacity-10 rounded-md active:bg-white p-1 mx-2 opacity-0 group-hover:opacity-100 group-hover:transition-opacity group-hover:duration-500'>
+        <button onClick={deleteIt} className='hover:bg-black hover:bg-opacity-10 rounded-md active:bg-white p-1 mx-2 opacity-0 group-hover:opacity-100 group-hover:transition-opacity group-hover:duration-500'>
         <TbTrash size={"2em"}/>
         </button>
       </div>
