65a5f2fde2a10ee3bb4bfd75ff99c3a46cb119ad
Claudio Parra <101138305+cdp50@users.noreply.github.com>
2023-12-26 12:56:06 -0700

    back to how it was

--- src/app/api/auth/[auth0]/route.js
+++ src/app/api/auth/[auth0]/route.js
@@ -1,16 +1,4 @@
 // app/api/auth/[auth0]/route.js
-import { handleAuth, handleCallback, handleLogin } from '@auth0/nextjs-auth0';
+import { handleAuth } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth({
-  async callback(req, res) {
-    try {
-      await handleCallback(req, res, {
-        // Set your custom callback URL here
-        returnTo: 'https://plums-sepia.vercel.app/welcome',
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(error.status || 500).end(error.message);
-    }
-  },
-});
+export const GET = handleAuth();
